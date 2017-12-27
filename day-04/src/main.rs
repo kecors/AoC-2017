3df503462f50fd5d5dd7324f52ab7185ba1332cf
Kenneth Collins <dev@kecors.net>
2017-12-27 13:11:34 -0500

    Solve day 4 part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -1,6 +1,27 @@
+//
+// To run part 1:
+//     cat puzzle-input.txt | cargo run
+//
+// To run part 2:
+//     cat puzzle-input.txt | cargo run --features part2
+//
+
 use std::io::{stdin, Read};
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
+
+#[cfg(feature="part2")]
+fn sort(word: &str) -> String {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort();
+    chars.into_iter().collect()
+}
+
+#[cfg(not(feature="part2"))]
+fn sort(word: &str) -> String {
+    // For part 1, do not sort
+    word.to_string()
+}
 
 //
 // Return true if each word in the passphrase is unique;
@@ -9,10 +30,11 @@ use std::collections::hash_map::Entry;
 fn check_passphrase(passphrase: &str) -> bool {
     let words: Vec<&str> = passphrase.split_whitespace().collect();
 
-    let mut hm = HashMap::<&str, bool>::new();
+    let mut hm = HashMap::<String, bool>::new();
 
     for word in words {
-        match hm.entry(word) {
+        let sorted_word = sort(word);
+        match hm.entry(sorted_word) {
             Entry::Occupied(_) => return false,
             Entry::Vacant(v)   => v.insert(true)
         };
