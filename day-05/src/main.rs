9033072a3bce6053ee1266b8de0a81c6d4736d48
Kenneth Collins <dev@kecors.net>
2017-12-27 17:21:07 -0500

    Solve day 5 part 1

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -1,3 +1,37 @@
+use std::io::{stdin, Read};
+
+fn jump_through_maze(instructions: &mut Vec<i32>) -> u32 {
+    let mut instruction: i32;
+    let mut index: usize = 0;
+    let mut new_index: i32;
+    let mut steps: u32 = 0;
+
+    loop {
+        instruction = instructions[index];
+        steps += 1;
+
+        new_index = (index as i32) + instruction;
+
+        // Determine if we have exited the maze
+        if new_index < 0 || new_index as usize >= instructions.len() {
+            break;
+        }
+
+        instructions[index] += 1;
+
+        index = new_index as usize;
+    }
+
+    steps
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    stdin().read_to_string(&mut input).unwrap();
+
+    let mut instructions: Vec<i32> = input.lines().map(|line| line.parse::<i32>().unwrap()).collect();
+
+    let steps: u32 = jump_through_maze(&mut instructions);
+    println!("steps = {}", steps);
 }
