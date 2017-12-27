74e16cc4c84a4ef25ec6f12d106e19798b9dd026
Kenneth Collins <dev@kecors.net>
2017-12-26 22:02:10 -0500

    Solve day 3, part 2; need to clean up code a bit

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -16,12 +16,12 @@ struct Square {
     x:         i32,
     y:         i32,
     direction: Direction,
-    sum:       Option<u32>
+    sum:       Option<u64>
 }
 
 impl Square {
     fn initial() -> Square {
-        Square { x: 0, y: 0, direction: Direction::RIGHT, sum: None }
+        Square { x: 0, y: 0, direction: Direction::RIGHT, sum: Some(1) }
     }
 
     fn create_next(&self) -> Square {
@@ -41,6 +41,62 @@ impl Square {
         };
 
         Square { x: x, y: y, direction: self.direction, sum: None }
+    }
+
+    fn summable_squares(&self, position: u32) -> Vec<(i32,i32)> {
+        let mut result : Vec<(i32,i32)> = Vec::new();
+
+        match self.direction {
+            Direction::RIGHT => {
+                result.push((self.x - 1, self.y    ));
+                result.push((self.x - 1, self.y + 1));
+            },
+            Direction::UP => {
+                result.push((self.x,     self.y - 1));
+                result.push((self.x - 1, self.y - 1));
+            },
+            Direction::LEFT => {
+                result.push((self.x + 1, self.y    ));
+                result.push((self.x + 1, self.y - 1));
+            },
+            Direction::DOWN => {
+                result.push((self.x,     self.y + 1));
+                result.push((self.x + 1, self.y + 1));
+            }
+        };
+        if position == 0 { return result; }
+
+        match self.direction {
+            Direction::RIGHT => {
+                result.push((self.x,     self.y + 1));
+            },
+            Direction::UP => {
+                result.push((self.x - 1, self.y    ));
+            },
+            Direction::LEFT => {
+                result.push((self.x,     self.y - 1));
+            },
+            Direction::DOWN => {
+                result.push((self.x + 1, self.y    ));
+            }
+        };
+        if position == 1 { return result; }
+        
+        match self.direction {
+            Direction::RIGHT => {
+                result.push((self.x + 1, self.y + 1));
+            },
+            Direction::UP => {
+                result.push((self.x - 1, self.y + 1));
+            },
+            Direction::LEFT => {
+                result.push((self.x - 1, self.y - 1));
+            },
+            Direction::DOWN => {
+                result.push((self.x + 1, self.y - 1));
+            }
+        };
+        return result;
     }
 
     fn turn(&mut self) {
@@ -68,10 +124,27 @@ fn build_grid(grid: &mut Vec<Square>, limit: usize) {
         grid.push(square);
 
         square = next_square;
+        step += 1;
 
         // Calcuate sum here
+        let ss = square.summable_squares(distance - step);
+        let mut sum = 0;
+//        println!("ss = {:?}", ss);
+        for f in grid.iter().rev() {
+            for g in ss.clone() {
+                if f.x == g.0 && f.y == g.1 {
+//                    println!("f = {:?}", f);
+                    sum += f.sum.unwrap();
+                }
+            }
+        }
+        square.sum = Some(sum);
+        if sum > (limit as u64) {
+            println!("sum = {}", sum);
+            std::process::exit(0);
+        }
 
-        step += 1;
+        // Turn the corner
         if step == distance {
             step = 0;
             if square.direction == Direction::UP || 
@@ -96,5 +169,5 @@ fn main() {
 
     build_grid(&mut grid, limit);
 
-    println!("steps for {} = {}", limit, grid[limit-1].calculate_steps());
+    println!("limit {}: steps = {}", limit, grid[limit-1].calculate_steps());
 }
