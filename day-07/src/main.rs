75b136af8c94d9ff2285bc6ea1955189e43cca92
Kenneth Collins <dev@kecors.net>
2018-01-01 11:47:55 -0500

    Add struct Tower and move functions inside impl Tower

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -29,6 +29,207 @@ impl Program {
     }
 }
 
+struct Tower {
+    bottom: String,
+    programs_hm: HashMap<String, Program>
+}
+
+impl Tower {
+    fn new(programs: Vec<Program>) -> Tower {
+        let bottom: String = Tower::find_bottom(&programs);
+
+        let mut programs_hm: HashMap<String, Program> = HashMap::new();
+        for program in programs {
+            programs_hm.insert(program.name.clone(), program);
+        }
+
+        Tower {
+            bottom: bottom,
+            programs_hm: programs_hm
+        }
+    }
+
+    fn find_bottom(programs: &Vec<Program>) -> String {
+        let mut programs_hs = HashSet::new();
+        for program in programs {
+            programs_hs.insert(program.name.clone());
+        }
+
+        let mut subprograms_hs = HashSet::new();
+        for program in programs {
+            for subprogram in program.disc.clone() {
+                subprograms_hs.insert(subprogram);
+            }
+        }
+
+        programs_hs.difference(&subprograms_hs).last().unwrap().clone()
+    }
+
+    fn calculate_disc_weights(&mut self) {
+        let mut stack: Vec<String> = Vec::new();
+
+        stack.push(self.bottom.clone());
+
+        loop {
+            if stack.len() == 0 {
+                break;
+            }
+
+            let name = stack.pop().unwrap();
+
+            let mut program = Program::default();
+            if let Some(p) = self.programs_hm.get(&name) {
+                program.name = p.name.clone();
+                program.weight = p.weight;
+                program.disc = p.disc.clone();
+            }
+
+            if program.disc.len() == 0 {
+                if let Some(p) = self.programs_hm.get_mut(&name) {
+                    p.disc_weight = Some(0);
+                    continue;
+                }
+            }
+
+            let mut needs_calculation = Vec::new();
+            for subname in program.disc.clone() {
+                if let Some(subprogram) = self.programs_hm.get(&subname) {
+                    if subprogram.disc_weight == None {
+                        needs_calculation.push(subname);
+                    }
+                }
+            }
+            if needs_calculation.len() > 0 {
+                stack.push(name);
+                for subname in needs_calculation {
+                    stack.push(subname);
+                }
+                continue;
+            }
+
+            let mut disc_weight = 0;
+            for subname in program.disc.clone() {
+                if let Some(subprogram) = self.programs_hm.get(&subname) {
+                    disc_weight += subprogram.weight;
+                    if let Some(subweight) = subprogram.disc_weight {
+                        disc_weight += subweight;
+                    }
+                }
+            }
+            if let Some(p) = self.programs_hm.get_mut(&name) {
+                p.disc_weight = Some(disc_weight);
+            }
+        }
+    }
+
+    fn display(&self) {
+        let mut stack: Vec<(String, u32)> = Vec::new();
+
+        stack.push((self.bottom.clone(), 0));
+
+        loop {
+            if stack.len() == 0 {
+                break;
+            }
+
+            let (name, depth) = stack.pop().unwrap();
+
+            let mut program = Program::default();
+            if let Some(p) = self.programs_hm.get(&name) {
+                program.name = p.name.clone();
+                program.weight = p.weight;
+                program.disc = p.disc.clone();
+                program.disc_weight = p.disc_weight;
+            }
+
+            for _ in 0..depth {
+                program.name.insert(0, '-');
+            }
+            print!("{:12} ({:6}) [{:6}]", program.name, 
+                                          program.weight, 
+                                          program.disc_weight.unwrap());
+
+            for subprogram in program.disc.clone() {
+                print!(" {}", subprogram);
+            }
+            println!("");
+
+            program.disc.reverse();
+            for subprogram in program.disc {
+                stack.push((subprogram, depth+1));
+            }
+        }
+    }
+
+    fn find_imbalance(&self) {
+        let mut stack = Vec::new();
+        let mut ancestry = Vec::new();
+        let mut standards = Vec::new();
+
+        stack.push(self.bottom.clone());
+
+        loop {
+            if stack.len() == 0 {
+                break;
+            }
+
+            let name = stack.pop().unwrap();
+
+            ancestry.push(name.clone());
+
+            let mut program = Program::default();
+            if let Some(p) = self.programs_hm.get(&name) {
+                program.name = p.name.clone();
+                program.weight = p.weight;
+                program.disc = p.disc.clone();
+                program.disc_weight = p.disc_weight;
+            };
+
+            let mut es: HashMap<u32, Vec<String>> = HashMap::new();
+            for subprogram in program.disc {
+                if let Some(p) = self.programs_hm.get(&subprogram) {
+                    match es.entry(p.total_weight()) {
+                        Entry::Vacant(v)   => { 
+                            println!("v = {:?}", v);
+                            let mut a = Vec::new();
+                            a.push(p.name.clone());
+                            v.insert(a); 
+                        },
+                        Entry::Occupied(mut o) => {
+                            println!("o = {:?}", o);
+                            o.get_mut().push(p.name.clone());
+                        }
+                    };
+                }
+            }
+            println!("es = {:?}", es);
+            println!("es.len() = {}", es.len());
+            if es.len() == 1 {
+                println!("ancestry = {:?}", ancestry);
+                println!("standards = {:?}", standards);
+                for (k,v) in es {
+                    let result = standards.pop().unwrap() - (k * v.len() as u32);
+                    println!("result = {}", result);
+                }
+                break;
+            }
+            let mut standard: u32 = 0;
+            let mut deviant: u32 = 0;
+            for (k,v) in es {
+                if v.len() == 1 {
+                    println!("v = {:?}", v);
+                    stack.push(v[0].clone());
+                    deviant = k;
+                } else {
+                    standard = k;
+                }
+            }
+            println!("standard = {}, deviant = {}", standard, deviant);
+            standards.push(standard);
+        }
+    }
+}
+
 fn parse_line(line: &str) -> Program {
     let mut program = Program::default();
 
@@ -48,186 +249,6 @@ fn parse_line(line: &str) -> Program {
     program
 }
 
-fn find_bottom(programs: &Vec<Program>) -> String {
-    let mut programs_hs = HashSet::new();
-    for program in programs {
-        programs_hs.insert(program.name.clone());
-    }
-
-    let mut subprograms_hs = HashSet::new();
-    for program in programs {
-        for subprogram in program.disc.clone() {
-            subprograms_hs.insert(subprogram);
-        }
-    }
-
-    programs_hs.difference(&subprograms_hs).last().unwrap().clone()
-}
-
-fn calculate_disc_weights(hm: &mut HashMap<String, Program>, bottom: String) {
-    let mut stack: Vec<String> = Vec::new();
-
-    stack.push(bottom);
-
-    loop {
-        if stack.len() == 0 {
-            break;
-        }
-
-        let name = stack.pop().unwrap();
-
-        let mut program = Program::default();
-        if let Some(p) = hm.get(&name) {
-            program.name = p.name.clone();
-            program.weight = p.weight;
-            program.disc = p.disc.clone();
-        }
-
-        if program.disc.len() == 0 {
-            if let Some(p) = hm.get_mut(&name) {
-                p.disc_weight = Some(0);
-                continue;
-            }
-        }
-
-        let mut needs_calculation = Vec::new();
-        for subname in program.disc.clone() {
-            if let Some(subprogram) = hm.get(&subname) {
-                if subprogram.disc_weight == None {
-                    needs_calculation.push(subname);
-                }
-            }
-        }
-        if needs_calculation.len() > 0 {
-            stack.push(name);
-            for subname in needs_calculation {
-                stack.push(subname);
-            }
-            continue;
-        }
-
-        let mut disc_weight = 0;
-        for subname in program.disc.clone() {
-            if let Some(subprogram) = hm.get(&subname) {
-                disc_weight += subprogram.weight;
-                if let Some(subweight) = subprogram.disc_weight {
-                    disc_weight += subweight;
-                }
-            }
-        }
-        if let Some(p) = hm.get_mut(&name) {
-            p.disc_weight = Some(disc_weight);
-        }
-    }
-}
-
-fn display_tower(hm: &HashMap<String, Program>, bottom: String) {
-    let mut stack: Vec<(String, u32)> = Vec::new();
-
-    stack.push((bottom, 0));
-
-    loop {
-        if stack.len() == 0 {
-            break;
-        }
-
-        let (name, depth) = stack.pop().unwrap();
-
-        let mut program = Program::default();
-        if let Some(p) = hm.get(&name) {
-            program.name = p.name.clone();
-            program.weight = p.weight;
-            program.disc = p.disc.clone();
-            program.disc_weight = p.disc_weight;
-        }
-
-        for _ in 0..depth {
-            program.name.insert(0, '-');
-        }
-        print!("{:12} ({:6}) [{:6}]", program.name, 
-                                      program.weight, 
-                                      program.disc_weight.unwrap());
-
-        for subprogram in program.disc.clone() {
-            print!(" {}", subprogram);
-        }
-        println!("");
-
-        program.disc.reverse();
-        for subprogram in program.disc {
-            stack.push((subprogram, depth+1));
-        }
-    }
-}
-
-fn find_imbalance(hm: &HashMap<String, Program>, bottom: String) {
-    let mut stack = Vec::new();
-    let mut ancestry = Vec::new();
-    let mut standards = Vec::new();
-
-    stack.push(bottom);
-
-    loop {
-        if stack.len() == 0 {
-            break;
-        }
-
-        let name = stack.pop().unwrap();
-
-        ancestry.push(name.clone());
-
-        let mut program = Program::default();
-        if let Some(p) = hm.get(&name) {
-            program.name = p.name.clone();
-            program.weight = p.weight;
-            program.disc = p.disc.clone();
-            program.disc_weight = p.disc_weight;
-        };
-
-        let mut es: HashMap<u32, Vec<String>> = HashMap::new();
-        for subprogram in program.disc {
-            if let Some(p) = hm.get(&subprogram) {
-                match es.entry(p.total_weight()) {
-                    Entry::Vacant(v)   => { 
-                        println!("v = {:?}", v);
-                        let mut a = Vec::new();
-                        a.push(p.name.clone());
-                        v.insert(a); 
-                    },
-                    Entry::Occupied(mut o) => {
-                        println!("o = {:?}", o);
-                        o.get_mut().push(p.name.clone());
-                    }
-                };
-            }
-        }
-        println!("es = {:?}", es);
-        println!("es.len() = {}", es.len());
-        if es.len() == 1 {
-            println!("ancestry = {:?}", ancestry);
-            println!("standards = {:?}", standards);
-            for (k,v) in es {
-                let result = standards.pop().unwrap() - (k * v.len() as u32);
-                println!("result = {}", result);
-            }
-            break;
-        }
-        let mut standard: u32 = 0;
-        let mut deviant: u32 = 0;
-        for (k,v) in es {
-            if v.len() == 1 {
-                println!("v = {:?}", v);
-                stack.push(v[0].clone());
-                deviant = k;
-            } else {
-                standard = k;
-            }
-        }
-        println!("standard = {}, deviant = {}", standard, deviant);
-        standards.push(standard);
-    }
-}
-
 fn main() {
     let mut input = String::new();
 
@@ -239,17 +260,14 @@ fn main() {
                                       .collect();
 //    println!("programs = {:?}", programs);
 
-    let bottom = find_bottom(&programs);
-//    println!("bottom = {}", bottom);
-
-    let mut hm: HashMap<String, Program> = HashMap::new();
-    for program in programs {
-        hm.insert(program.name.clone(), program);
-    }
+    let mut tower = Tower::new(programs);
+    tower.calculate_disc_weights();
+    tower.display();
+    tower.find_imbalance();
 //    println!("hm = {:?}", hm);
-    calculate_disc_weights(&mut hm, bottom.clone());
+//    calculate_disc_weights(&mut tower.programs_hm, tower.bottom.clone());
 //    println!("");
 //    println!("hm = {:?}", hm);
-    display_tower(&hm, bottom.clone());
-    find_imbalance(&hm, bottom.clone());
+//    display_tower(&tower.programs_hm, tower.bottom.clone());
+//    find_imbalance(&tower.programs_hm, tower.bottom.clone());
 }
