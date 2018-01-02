d09cdf2275b5923499ab0bb5efe0aa3a341712c7
Kenneth Collins <dev@kecors.net>
2018-01-02 14:42:02 -0500

    Solve day 10 part 2

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -2,7 +2,7 @@ use std::io;
 
 #[derive(Debug)]
 struct KnotHash {
-    list: Vec<usize>,
+    list: Vec<u32>,
     position: usize,
     skip: usize
 }
@@ -11,7 +11,7 @@ impl KnotHash {
     fn new(list_size: usize) -> KnotHash {
         let mut list = Vec::new();
         for j in 0..list_size {
-            list.push(j);
+            list.push(j as u32);
         }
         KnotHash {
             list: list,
@@ -48,15 +48,40 @@ impl KnotHash {
         self.skip += 1;
     }
 
-    fn product_first_two(&self) -> usize {
+    fn product_first_two(&self) -> u32 {
         self.list[0] * self.list[1]
     }
 }
 
-fn main() {
-    let mut input = String::new();
+// Assuming spare hash has 256 elements
+fn calculate_dense_hash(spare_hash: &Vec<u32>) -> Vec<u32> {
+    let mut dense_hash: Vec<u32> = Vec::new();
 
-    io::stdin().read_line(&mut input).unwrap();
+    let mut sp = spare_hash.clone();
+    for _ in 0..16 {
+        let remainder = sp.split_off(16);
+        let k = sp.pop().unwrap();
+        let result = sp.iter().fold(k, |acc, &x| acc ^ x);
+        dense_hash.push(result);
+        sp = remainder;
+    }
+
+    dense_hash
+}
+
+// Assuming dense hash has 16 elements
+fn calculate_hexadecimal_string(dense_hash: &Vec<u32>) -> String {
+    let mut result = String::new();
+
+    for value in dense_hash {
+        let h = format!("{:02x}", value);
+        result.push_str(h.as_str());
+    }
+
+    result
+}
+
+fn do_part1(input: &String) {
     let strs: Vec<&str> = input.trim()
                                .split(',')
                                .collect();
@@ -72,6 +97,40 @@ fn main() {
     println!("product first two = {}", kh.product_first_two());
 }
 
+fn do_part2(input: &String) -> String {
+//    let mut lengths = ascii(input.as_str());
+    let mut lengths: Vec<u8> = Vec::new();
+    lengths.extend(input.as_bytes());
+    lengths.extend(&[17,31,73,47,23]);
+//    println!("lengths = {:?}", lengths);
+
+    let mut kh = KnotHash::new(256);
+    for _ in 0..64 {
+        for length in lengths.clone() {
+            let p = length as usize;
+            kh.twist(p);
+        }
+    }
+//    println!("kh = {:?}", kh);
+//    println!("product first two = {}", kh.product_first_two());
+
+    let dh = calculate_dense_hash(&kh.list);
+//    println!("dh = {:?}", dh);
+    let hs = calculate_hexadecimal_string(&dh);
+    return hs
+}
+
+fn main() {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    do_part1(&input);
+
+    let result = do_part2(&input);
+    println!("part 2 result = {}", result);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -84,5 +143,33 @@ mod tests {
         }
         println!("kh = {:?}", kh);
         assert_eq!(12, kh.product_first_two());
+    }
+
+    #[test]
+    fn try_part2_1() {
+        let x = "".to_string();
+        let z = do_part2(&x);
+        assert_eq!(z, "a2582a3a0e66e6e86e3812dcb672a272");
+    }
+
+    #[test]
+    fn try_part2_2() {
+        let x = "AoC 2017".to_string();
+        let z = do_part2(&x);
+        assert_eq!(z, "33efeb34ea91902bb2f59c9920caa6cd");
+    }
+
+    #[test]
+    fn try_part2_3() {
+        let x = "1,2,3".to_string();
+        let z = do_part2(&x);
+        assert_eq!(z, "3efbe78a8d82f29979031a4aa0b16a9d");
+    }
+
+    #[test]
+    fn try_part2_4() {
+        let x = "1,2,4".to_string();
+        let z = do_part2(&x);
+        assert_eq!(z, "63960835bcdc130f0b66d7ff4f6a5a8e");
     }
 }
