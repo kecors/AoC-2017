1c446e212a01cc0980672c0ac737a3085926bf50
Kenneth Collins <dev@kecors.net>
2018-01-03 11:33:06 -0500

    Solve day 11 part 1

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -1,3 +1,77 @@
+use std::io;
+
+#[derive(Debug)]
+enum Direction {
+    N,
+    NE,
+    SE,
+    S,
+    SW,
+    NW
+}
+
+#[derive(Debug, Default)]
+struct State {
+    x: i32,
+    y: i32,
+    z: i32
+}
+
+impl State {
+    fn new() -> State {
+        State::default()
+    }
+
+    fn go(&mut self, direction: Direction) {
+        match direction {
+            Direction::N  => { self.x += 1; self.z -= 1; },
+            Direction::NE => { self.x += 1; self.y -= 1; },
+            Direction::SE => { self.z += 1; self.y -= 1; },
+            Direction::S  => { self.z += 1; self.x -= 1; },
+            Direction::SW => { self.y += 1; self.x -= 1; },
+            Direction::NW => { self.y += 1; self.z -= 1; },
+        }
+    }
+
+    fn fewest_steps(&self) -> i32 {
+        *[self.x.abs(), self.y.abs(), self.z.abs()].iter().max().unwrap()
+    }
+}
+
+fn parse_line(input: &str) -> Vec<Direction> {
+    let mut steps = Vec::new();
+
+    let directions: Vec<&str> = input.split(',').collect();
+    for direction in directions {
+        match direction {
+            "n"  => { steps.push(Direction::N); },
+            "ne" => { steps.push(Direction::NE); },
+            "se" => { steps.push(Direction::SE); },
+            "s"  => { steps.push(Direction::S); },
+            "sw" => { steps.push(Direction::SW); },
+            "nw" => { steps.push(Direction::NW); },
+            _    => { }
+        }
+    }
+
+    steps
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+//    println!("input = {:?}", input);
+
+    let directions = parse_line(input.trim());
+//    println!("directions = {:?}", directions);
+
+    let mut state = State::new();
+//    println!("state = {:?}", state);
+
+    for direction in directions {
+        state.go(direction);
+    }
+//    println!("state = {:?}", state);
+    println!("fewest steps = {}", state.fewest_steps());
 }
