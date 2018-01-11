f68ad6a14464cfdcd8f6535e5ef76bcb2607ef51
Kenneth Collins <dev@kecors.net>
2018-01-10 22:49:31 -0500

    Solve day 22 part 2

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -1,12 +1,19 @@
 use std::io::{stdin, Read};
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 #[derive(Debug)]
 enum Direction {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+#[derive(Debug, Clone)]
+enum Condition {
+    Weakened,
+    Infected,
+    Flagged
 }
 
 #[derive(Debug, Eq, PartialEq, Hash, Clone)]
@@ -17,46 +24,105 @@ struct Position {
 
 #[derive(Debug)]
 struct State {
-    infected: HashSet<Position>,
+    unclean: HashMap<Position, Condition>,
     position: Position,
     direction: Direction,
     infection: u32
 }
 
 impl State {
-    fn new(infected: HashSet<Position>) -> State {
+    fn new(unclean: HashMap<Position, Condition>) -> State {
         State {
-            infected: infected,
+            unclean: unclean,
             position: Position { x: 0, y: 0 },
-            direction: Direction::UP,
+            direction: Direction::Up,
             infection: 0
         }
     }
 
-    fn burst(&mut self) {
-        if self.infected.contains(&self.position) {
+    fn burst_part_1(&mut self) {
+        let mut remove_flag = false;
+        let mut insert_infected_flag = false;
+        if let Some(_condition) = self.unclean.get(&self.position) {
             self.direction = match self.direction {
-                Direction::UP    => Direction::RIGHT,
-                Direction::RIGHT => Direction::DOWN,
-                Direction::DOWN  => Direction::LEFT,
-                Direction::LEFT  => Direction::UP
+                Direction::Up    => Direction::Right,
+                Direction::Right => Direction::Down,
+                Direction::Down  => Direction::Left,
+                Direction::Left  => Direction::Up
             };
-            self.infected.remove(&self.position);
+            remove_flag = true;
         } else {
             self.direction = match self.direction {
-                Direction::UP    => Direction::LEFT,
-                Direction::LEFT  => Direction::DOWN,
-                Direction::DOWN  => Direction::RIGHT,
-                Direction::RIGHT => Direction::UP
+                Direction::Up    => Direction::Left,
+                Direction::Left  => Direction::Down,
+                Direction::Down  => Direction::Right,
+                Direction::Right => Direction::Up
             };
-            self.infected.insert(self.position.clone());
+            insert_infected_flag = true;
             self.infection += 1;
         }
+        if remove_flag {
+            self.unclean.remove(&self.position);
+        }
+        if insert_infected_flag {
+            self.unclean.insert(self.position.clone(), Condition::Infected);
+        }
         match self.direction {
-            Direction::UP    => { self.position.y += 1; },
-            Direction::RIGHT => { self.position.x += 1; },
-            Direction::DOWN  => { self.position.y -= 1; },
-            Direction::LEFT  => { self.position.x -= 1; }
+            Direction::Up    => { self.position.y += 1; },
+            Direction::Right => { self.position.x += 1; },
+            Direction::Down  => { self.position.y -= 1; },
+            Direction::Left  => { self.position.x -= 1; }
+        }
+    }
+
+    fn burst_part_2(&mut self) {
+        let mut remove_flag = false;
+        let mut insert_weakened_flag = false;
+        if let Some(condition) = self.unclean.get_mut(&self.position) {
+            match *condition {
+                Condition::Weakened => {
+                    *condition = Condition::Infected;
+                    self.infection += 1;
+                },
+                Condition::Infected => {
+                    self.direction = match self.direction {
+                        Direction::Up    => Direction::Right,
+                        Direction::Right => Direction::Down,
+                        Direction::Down  => Direction::Left,
+                        Direction::Left  => Direction::Up
+                    };
+                    *condition = Condition::Flagged;
+                },
+                Condition::Flagged => {
+                    self.direction = match self.direction {
+                        Direction::Up    => Direction::Down,
+                        Direction::Right => Direction::Left,
+                        Direction::Down  => Direction::Up,
+                        Direction::Left  => Direction::Right
+                    };
+                    remove_flag = true;
+                }
+            }
+        } else {
+            self.direction = match self.direction {
+                Direction::Up    => Direction::Left,
+                Direction::Left  => Direction::Down,
+                Direction::Down  => Direction::Right,
+                Direction::Right => Direction::Up
+            };
+            insert_weakened_flag = true;
+        }
+        if remove_flag {
+            self.unclean.remove(&self.position);
+        }
+        if insert_weakened_flag {
+            self.unclean.insert(self.position.clone(), Condition::Weakened);
+        }
+        match self.direction {
+            Direction::Up    => { self.position.y += 1; },
+            Direction::Right => { self.position.x += 1; },
+            Direction::Down  => { self.position.y -= 1; },
+            Direction::Left  => { self.position.x -= 1; }
         }
     }
 }
@@ -66,7 +132,7 @@ fn main() {
 
     stdin().read_to_string(&mut input).unwrap();
 
-    let mut hs: HashSet<Position> = HashSet::new();
+    let mut hm: HashMap<Position, Condition> = HashMap::new();
     let width = input.lines().count() as i32;
     let mut x: i32;
     let mut y: i32 = width / 2;
@@ -74,19 +140,22 @@ fn main() {
         x = -1 * (width / 2);
         for c in line.chars() {
             if c == '#' {
-                hs.insert(Position { x, y });
+                hm.insert(Position { x, y }, Condition::Infected);
             }
             x += 1;
         }
         y -= 1;
     }
 
-    let mut state = State::new(hs);
-//    println!("state = {:?}", state);
-
+    let mut state = State::new(hm.clone());
     for _ in 0..10000 {
-        state.burst();
+        state.burst_part_1();
     }
-//    println!("state = {:?}", state);
     println!("part 1: infections = {}", state.infection);
+
+    let mut state = State::new(hm);
+    for _ in 0..10000000 {
+        state.burst_part_2();
+    }
+    println!("part 2: infections = {}", state.infection);
 }
